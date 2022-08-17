8ee3545dacbba99e0b638089c07c0040be770c84
Shahar Rosen <shaharr@wix.com>
2022-08-17 23:25:56 +0300

    bob

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -13,7 +13,6 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
